d8c233cf4658ffc53cc35621ab81d4cd800e892b
lap <tuanminhphan45@gmail.com>
2025-05-12 09:52:15 +0700

    eval

---
+++ run_scripts/blip2_timesformer/eval/eval.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export MASTER_PORT=55555
+export MASTER_ADDR=localhost
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+export LAVIS_CACHE_ROOT="/storage/student10/vidcaption/LAVIS/cache"
+cd /storage/student10/vidcaption/LAVIS
+export PYTHONPATH=/storage/student10/vidcaption/LAVIS:$PYTHONPATH
+
+python -m torch.distributed.run --nproc_per_node=8 evaluate.py --cfg-path lavis/projects/alpro/eval/msrvtt_qa_eval.yaml
