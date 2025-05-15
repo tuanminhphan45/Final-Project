b8274cc13bf83fa48257960daf15c723038d5098
lap <tuanminhphan45@gmail.com>
2025-05-15 09:26:33 +0700

    ok

--- run_scripts/blip2_timesformer/eval/eval.sh
+++ run_scripts/blip2_timesformer/eval/eval.sh
@@ -7,4 +7,4 @@ export LAVIS_CACHE_ROOT="/storage/student10/vidcaption/LAVIS/cache"
 cd /storage/student10/vidcaption/LAVIS
 export PYTHONPATH=/storage/student10/vidcaption/LAVIS:$PYTHONPATH
 
-python -m torch.distributed.run --nproc_per_node=8 evaluate.py --cfg-path lavis/projects/blip2_timesformer/eval/eval_msrvtt.yaml
+python -m torch.distributed.run --nproc_per_node=3 evaluate.py --cfg-path lavis/projects/blip2_timesformer/eval/eval_msrvtt.yaml
