518014dc14581ef3ded83dcd9eac091d381db08b
lap <tuanminhphan45@gmail.com>
2025-05-15 09:19:37 +0700

    ok

--- run_scripts/blip2_timesformer/eval/eval.sh
+++ run_scripts/blip2_timesformer/eval/eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export MASTER_PORT=55555
 export MASTER_ADDR=localhost
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=1,2,3
 
 export LAVIS_CACHE_ROOT="/storage/student10/vidcaption/LAVIS/cache"
 cd /storage/student10/vidcaption/LAVIS
